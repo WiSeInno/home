b8008360aae98d24ace97895d45931d962b28690
Dimitris <xamenosstr@gmail.com>
2018-09-18 22:25:23 +0300

    adds social links

--- src/components/hero.vue
+++ src/components/hero.vue
@@ -3,11 +3,17 @@
     <h1 class="title">WiSeInno</h1>
     <h2 class="subtitle">Innovation is fun</h2>
     <div class="icon_bar">
-      <a>
-        <el-button type='primary' icon='el-icon-search' circle></el-button>
+      <a href="https://twitter.com/wise_inno">
+        <i class="fab fa-twitter fa-2x"></i>
       </a>
-      <a>
-        <el-button type='primary' icon='el-icon-edit' circle></el-button>
+      <a href="https://facebook.com/wiseinno.tech">
+        <i class="fab fa-facebook fa-2x"></i>
+      </a>
+      <a href="mailto:info@wiseinno.tech">
+        <i class="fas fa-envelope fa-2x"></i>
+      </a>
+      <a href="https://wiseinno-events.herokuapp.com">
+        <i class="fas fa-calendar-alt fa-2x"></i>
       </a>
     </div>
   </div>
@@ -36,6 +42,12 @@ export default {
       color: whitesmoke;
     }
     .icon_bar{
+      * {
+        margin-right: 5px;
+      }
+      a{
+        color: #fffff2;
+      }
       i{
         font-size: 2rem;
       }
