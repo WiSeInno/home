d735ec91657cd175eaeb54dd40b34a28002ef0ac
Dimitris <xamenosstr@gmail.com>
2018-09-18 21:55:06 +0300

    fixes static path for the builds

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/wiseinno.tech/',
+    assetsPublicPath: '/home/',
 
     /**
      * Source Maps
